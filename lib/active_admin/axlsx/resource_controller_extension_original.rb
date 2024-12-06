23226e95761f507350cfa9e00b3ccb86b267ff1f
Jasmin Zulic <jzulic@dmgov.org>
2024-12-06 15:27:25 -0600

    Method changed.

--- lib/active_admin/axlsx/resource_controller_extension_original.rb
+++ lib/active_admin/axlsx/resource_controller_extension_original.rb
@@ -2,34 +2,27 @@ module ActiveAdmin
   module Axlsx
     module ResourceControllerExtension
       def self.included(base)
-        base.send :alias_method, :per_page_without_xlsx, :xlsx
-        base.send :alias_method, :xlsx, :per_page_with_xlsx
-        base.send :respond_to, :xlsx, only: :index
+        base.send :alias_method_chain, :per_page, :xlsx
+        base.send :alias_method_chain, :index, :xlsx
         base.send :respond_to, :xlsx
       end
 
       # patching the index method to allow the xlsx format.
-      # Patches index to respond to requests with xls mime type by
-      # sending a generated xls document serializing the current
-      # collection
-      def index
-        super do |format|
-          format.xlsx do
+      def index_with_xlsx(options={}, &block)
+        index_without_xlsx(options) do |format|
+           format.xlsx do
             xlsx = active_admin_config.xlsx_builder.serialize(collection)
-            send_data(xlsx,
-                      :filename  => "#{xlsx_filename}",
-                      type: Mime::Type.lookup_by_extension(:xlsx))
+            send_data xlsx, :filename => "#{xlsx_filename}", :type => Mime::Type.lookup_by_extension(:xlsx)
           end
-
-          yield(format) if block_given?
         end
       end
 
       # patching per_page to use the CSV record max for pagination when the format is xlsx
       def per_page_with_xlsx
-        if request.format ==  Mime::Type.lookup_by_extension(:xlsx)
-          return max_csv_records
-        end
+          if request.format ==  Mime::Type.lookup_by_extension(:xlsx)
+            return max_csv_records
+          end
+          per_page_without_xlsx
       end
 
       # Returns a filename for the xlsx file using the collection_name
