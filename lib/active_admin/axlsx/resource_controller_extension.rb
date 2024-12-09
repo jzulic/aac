4ea0961af13cb309b5f132ba9eb9f4a856face4a
Jasmin Zulic <jzulic@dmgov.org>
2024-12-09 13:25:58 -0600

    Method changed.

--- lib/active_admin/axlsx/resource_controller_extension.rb
+++ lib/active_admin/axlsx/resource_controller_extension.rb
@@ -2,12 +2,8 @@ module ActiveAdmin
   module Axlsx
     module ResourceControllerExtension
       def self.included(base)
-        # base.send :alias_method_chain, :per_page, :xlsx
-        # base.send :alias_method_chain, :index, :xlsx
         base.send :alias_method, :per_page_without_xlsx, :per_page
         base.send :alias_method, :per_page, :per_page_with_xlsx
-        # base.send :alias_method, :index_without_xlsx, :index
-        # base.send :alias_method, :index, :index_with_xlsx
         base.send :respond_to, :xlsx
       end
 
@@ -28,16 +24,6 @@ module ActiveAdmin
           yield(format) if block_given?
         end
       end
-
-      # patching the index method to allow the xlsx format.
-      # def index_with_xlsx(options={}, &block)
-      #   index_without_xlsx(options) do |format|
-      #      format.xlsx do
-      #       xlsx = active_admin_config.xlsx_builder.serialize(collection)
-      #       send_data xlsx, :filename => "#{xlsx_filename}", :type => Mime::Type.lookup_by_extension(:xlsx)
-      #     end
-      #   end
-      # end
 
       # patching per_page to use the CSV record max for pagination when the format is xlsx
       def per_page_with_xlsx
